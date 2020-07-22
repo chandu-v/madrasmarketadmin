48bd1aa176e7c5e79baa611d4f7b29cc4d75d451
chandu <chandu_boss@hotmail.com>
2020-07-22 19:22:13 +0530

    'update'

--- deploy.bat
+++ deploy.bat
@@ -1,5 +1,20 @@
+# npm run build-prod
+pause
 cp .\web.config .\dist\madras-market-admin\
+pause
 mv .\dist\madras-market-admin\
+pause
+git init
+pause
+git remote add origin https://github.com/chandu-v/madrasmarketplaceadmin-deploy
+pause
+git config user.email chandu_boss@hotmail.com
+pause
+git config user.name chandu
+pause
 git add .
+pause
 git commit -m 'update'
+pause
 git push -f
+pause
