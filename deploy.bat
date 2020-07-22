298b482a55cd247feb5ea5f7397e775875328f85
chandu <chandu_boss@hotmail.com>
2020-07-22 20:47:13 +0530

    Change in deploy.bat to take commit message dynamically

--- deploy.bat
+++ deploy.bat
@@ -7,6 +7,6 @@ git remote add origin https://github.com/chandu-v/madrasmarketplaceadmin-deploy
 git config user.email chandu_boss@hotmail.com
 git config user.name chandu
 git add .
-git commit -m 'update'
+git commit -a
 git push --set-upstream origin master -f
 pause
