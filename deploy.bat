2bb375de380fbf8e2aaa31fad4780fdf0380bbe5
chandu <chandu_boss@hotmail.com>
2020-07-22 19:26:02 +0530

    'update'

--- deploy.bat
+++ deploy.bat
@@ -1,18 +1,9 @@
-# npm run build-prod
-pause
 copy .\web.config .\dist\madras-market-admin\
-pause
 git init
-pause
 git remote add origin https://github.com/chandu-v/madrasmarketplaceadmin-deploy
-pause
 git config user.email chandu_boss@hotmail.com
-pause
 git config user.name chandu
-pause
 git add .
-pause
 git commit -m 'update'
-pause
-git push -f
+git push --set-upstream origin master -f
 pause
